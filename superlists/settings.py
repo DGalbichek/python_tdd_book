165cec2750edcbe333dbeae98ba04c23ccc594f8
DGalbichek <david.galbicsek@gmail.com>
2022-08-23 22:05:57 +0100

    Ch10

--- superlists/settings.py
+++ superlists/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,14 +19,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a%!-)_v9qm+892l%#c#7l!ydy&+u&5r!t#)ur5*v60o4-uyihb'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*', ]
-
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME'], ]
+else:
+    DEBUG = True
+    SECRET_KEY = 'django-insecure-a%!-)_v9qm+892l%#c#7l!ydy&+u&5r!t#)ur5*v60o4-uyihb'
+    ALLOWED_HOSTS = ['*', ]
 
 # Application definition
 
